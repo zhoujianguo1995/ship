679f0fd1593cb1c76a79d85f54628019f2032b71
zhoujianguo <470866854@qq.com>
2019-06-28 16:24:55 +0800

    登入功能基本完成

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,16 +1,23 @@
 <template>
   <el-row type="flex" class="row-bg rowbox" justify="center" align="middle">
     <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
-      <el-form ref="form" :model="form" label-width="80px" label-position="top">
-        <el-form-item label="用户名">
-          <el-input v-model="form.username"></el-input>
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+        label-position="top"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="ruleForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
-          <el-input v-model="form.password"></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="ruleForm.password" type="password" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">登录</el-button>
-          <el-button>重置</el-button>
+          <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -18,34 +25,65 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      form: {
+      ruleForm: {
         username: "admin",
         password: "123456"
+      },
+      rules: {
+        username: [
+          { required: true, message: "账户", trigger: "blur" },
+          { min: 3, max: 8, message: "长度在 3 到 8 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输密码", trigger: "blur" },
+          { min: 3, max: 11, message: "长度在 3 到 11 个字符", trigger: "blur" }
+        ]
       }
     };
   },
   methods: {
-    onSubmit() {
-      console.log("submit!");
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          axios({
+            url: "http://localhost:8888/api/private/v1/login",
+            method: "post",
+            data: this.ruleForm
+          }).then(({ data: { data, meta } }) => {
+            if (meta.status == 200) {
+              localStorage.setItem("token", data.token);
+              this.$router.push("/home");
+            } else {
+              alert("账号或者密码错误");
+            }
+          });
+        } else {
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     }
   }
 };
 </script>
 
 <style lang="less">
-  .el-form {
-    min-width: 400px;
-    padding: 30px 20px;
-    background-color: #fff;
-    border-radius: 10px;
-  }
+.el-form {
+  min-width: 400px;
+  padding: 30px 20px;
+  background-color: #fff;
+  border-radius: 10px;
+}
 
-  .rowbox {
-    height: 100%;
-    width: 100%;
-    background-color: #2D434C;
-  }
+.rowbox {
+  height: 100%;
+  width: 100%;
+  background-color: #2d434c;
+}
 </style>
